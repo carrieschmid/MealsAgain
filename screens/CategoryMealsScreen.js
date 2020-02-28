f9b0ccfb3d2e0ecd718ec09410b4c87161572057
Carrie Schmid <carriepederson7@gmail.com>
2020-02-28 15:06:08 -0800

    Add back buttons.

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
 
-const CategoryMealScreen = props => {
+const CategoryMealScreen = (props) => {
   return (
     <View style={styles.screen}>
       <Text>The Category Meal Screen!</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => {
+          props.navigation.navigate({ routeName: "CategoryMeals" });
+          // props.navigation.push("Categories");
+          //push useful to go to a page you're already on, change contents on same screen
+          //.replace() takes you to screen and empties stack, no back button
+        }}
+      />
+      <Button
+        title="Go Back"
+        onPress={() => {
+          props.navigation.goBack();
+          //pop() pops current button off stack, when you're in stack
+        }}
+      />
     </View>
   );
 };
@@ -12,8 +28,8 @@ const CategoryMealScreen = props => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: "center",
+    alignItems: "center"
   }
 });
 
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
 
-const MealDetailScreen = props => {
+const MealDetailScreen = (props) => {
   return (
     <View style={styles.screen}>
       <Text>The Meal Detail Screen!</Text>
+      <Button
+        title="Go Back to Categories"
+        onPress={() => {
+          props.navigation.popToTop();
+          //popToToptaks back to root screen
+        }}
+      />
     </View>
   );
 };
@@ -12,8 +19,8 @@ const MealDetailScreen = props => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: "center",
+    alignItems: "center"
   }
 });
 
