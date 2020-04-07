386ed5003507a4d14166149037322a0a3465ea65
Carrie Schmid <carriepederson7@gmail.com>
2020-04-06 17:54:27 -0700

    Add filters to redux

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 import { useSelector } from "react-redux";
 import { CATEGORIES } from "../data/dummy-data";
 import MealList from "../components/MealList";
@@ -13,6 +14,14 @@ const CategoryMealScreen = (props) => {
     (meal) => meal.categoryIds.indexOf(catId) >= 0
   );
 
+  if (displayedMeals.length === 0) {
+    return (
+      <View style={styles.content}>
+        <Text> No meals founds, maybe check your filters.</Text>
+      </View>
+    );
+  }
+
   return <MealList listData={displayedMeals} navigation={props.navigation} />;
 };
 
@@ -23,5 +32,13 @@ CategoryMealScreen.navigationOptions = (navigationData) => {
     headerTitle: selectedCategory.title
   };
 };
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
 
 export default CategoryMealScreen;
--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
+
 import { useSelector } from "react-redux";
 
 import HeaderButton from "../components/HeaderButton";
@@ -9,6 +10,13 @@ import { MEALS } from "../data/dummy-data";
 
 const FavoritesScreen = (props) => {
   const favMeals = useSelector((state) => state.meals.favoriteMeals);
+  if (favMeals.length === 0 || !favMeals) {
+    return (
+      <View style={styles.content}>
+        <Text>No Favorite Meals Found. Start adding some!</Text>
+      </View>
+    );
+  }
   return <MealList listData={favMeals} navigation={props.navigation} />;
 };
 
@@ -29,12 +37,12 @@ FavoritesScreen.navigationOptions = (navData) => {
   };
 };
 
-// const styles = StyleSheet.create({
-//   screen: {
-//     flex: 1,
-//     justifyContent: "center",
-//     alignItems: "center"
-//   }
-// });
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
 
 export default FavoritesScreen;
